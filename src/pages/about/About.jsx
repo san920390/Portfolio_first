2e737ecb9a3972c300614c70ed94a26ed8395718
san920390 <sandeephaldar4888@gmail.com>
2023-09-17 12:03:34 +0530

    education and experience in skill section

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,7 +4,9 @@ import Info from '../../components/Info'
 import { FaDownload } from 'react-icons/fa'
 import CV from '../../assets/steve-Cv.pdf'
 import Skills from '../../components/Skills'
+import { resume } from '../../data'
 import './about.css'
+import ResumeItem from '../../components/ResumeItem'
 
 const About = () => {
   return (
@@ -48,6 +50,40 @@ const About = () => {
         <div className='skills__container grid'>
           <Skills/>
         </div>.
+      </section>
+
+      <div className="seprator"></div>
+
+      <section className="resume">
+        <h3 className="section__subtitle subtitle__center">
+          Experience & Education
+        </h3>
+
+        <div className="resume__container grid">
+          <div className="resume__data">
+            {
+              resume.map((val)=>{
+              if (val.category === 'experience'){
+                return (
+                  <ResumeItem key={val.id} {...val}/>
+                )
+              }
+            })
+            }
+          </div>
+
+          <div className="resume__data">
+            {
+              resume.map((val)=>{
+              if (val.category === 'education'){
+                return (
+                  <ResumeItem key={val.id} {...val}/>
+                )
+              }
+            })
+            }
+          </div>
+        </div>
       </section>
     </main>
   )
